13c794e1a2827dc7dd9d73c74db6117cfa2b8f08
Michael V. Colianna <michael.colianna@me.com>
2024-12-12 11:47:07 -0500

    Adds SEO and anti-ai stuff

---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,22 @@
+<script>
+  let { page } = $props();
+</script>
+
+<svelte:head>
+  {#if page.content.component === 'page'}
+    <title>The Guild Library Appendix</title>
+    <meta name="description" content="The home page for the Guild Library Appendix, with a list of categories" />
+  {/if}
+
+  {#if page.content.component === 'listing'}
+    <title>Category: {page.name} | The Guild Library Appendix</title>
+    <meta name="description" content={page.content.content} />
+  {/if}
+
+  {#if page.content.component === 'entry'}
+    <title>Entry: {page.content.displayName} | The Guild Library Appendix</title>
+    <meta name="description" content={page.content.excerpt} />
+  {/if}
+
+  <meta name="robots" content="noai, noimageai" />
+</svelte:head>
--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -2,11 +2,14 @@
   import Entry from '$lib/components/Entry.svelte';
   import Listing from '$lib/components/Listing.svelte';
   import Page from '$lib/components/Page.svelte';
+  import Seo from '$lib/components/Seo.svelte';
 
   let { data } = $props();
 
   let page = $derived(data.page);
 </script>
+
+<Seo {page} />
 
 {#if page.content.component === 'page'}
   <Page {page} />
